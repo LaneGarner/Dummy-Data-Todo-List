85571fd97c86ffdd17261684e5ac88e24b9c0721
Lane Garner <b.lane.garner@gmail.com>
2020-10-11 19:08:52 -0500

    filtering working

--- scripts.js
+++ scripts.js
@@ -26,6 +26,8 @@ let arrayOfTodos = [
     console.log(arrayOfTodos)
   }
 
+  // const arrayFilter = arrayOfTodos.filter(array => array.userId == myID);
+
 
   
 
@@ -38,12 +40,19 @@ let arrayOfTodos = [
         // addLI.appendChild(text);
 
         let addLI = document.createElement('li');
+        // let doneButton = document.createElement('button');
         addLI.innerHTML = arrayOfTodos[i].title;
         toDos.appendChild (addLI);
 
       if (arrayOfTodos[i].completed == false) {
           addLI.classList.add('incomplete');
           addLI.setAttribute("id", i);
+          // li.appendChild(doneButton)
+          // doneButton.innerText = "done"
+          // doneButton.classList.add("done-button")
+
+
+          
         //   const checkMark = addLI.insertAdjacentHTML('beforeend', ' <i class="far fa-check-circle"></i>');
         // //   checkMark.classList.add(i);
         //   checkMark.addEventListener('mouseup', function () {
@@ -53,6 +62,13 @@ let arrayOfTodos = [
       } else {
           addLI.classList.add('complete');
       } 
+
+   //   doneButton.addEventListener('click', function () {
+    //     console.log("clicked")
+    //     li.style.color = "black"
+    //     li.style.textDecoration = "line-through"
+    //     li.removeChild(doneButton)
+    // })
     // toDos.appendChild(addLI);
   }
 }
@@ -61,33 +77,99 @@ let arrayOfTodos = [
 //   document.getElementById("todo-list").innerHTML = "";
 // }
 
+
+
 const findID = () => {
   let toDos = document.getElementById('todo-list');
   document.getElementById("todo-list").innerHTML = "";
-  // toDos.innerHTML = "";
+  toDos.innerHTML = "";
   const myID = document.getElementById('user').value;
   console.log(myID);
-  arrayFilter = arrayOfTodos.filter(array => array.userId == myID);
-  
-  for (let i = 0; i < arrayFilter.length; i++) {  
-  //  let addLI = document.createElement('li');
-  //  let text = document.createTextNode(arrayOfTodos[i].title);
-  //   addLI.appendChild(text); 
-    
-    
+  const arrayFilter = arrayOfTodos.filter(array => array.userId == myID);
+  // console.log(arrayFilter);
+
+  for (let i = 0; i < arrayFilter.length; i++) {
     let addLI = document.createElement('li');
-    addLI.innerHTML = arrayFilter;
+    addLI.innerHTML = arrayFilter[i].title;
     toDos.appendChild(addLI);
+    console.log(arrayFilter[i].completed);
+    
+    if (arrayOfTodos[i].completed == false) {
+      addLI.classList.add('incomplete');
+      addLI.setAttribute("id", i);
+    } else {
+        addLI.classList.add('complete');
+    } 
   }
 }
 
 
-// const filterTodos = () => {
-  //   const myID = document.getElementById('user');
 
-  //   arrayOfTodos.userID
 
+const showComplete = () => {
+  const x = document.querySelectorAll('.incomplete');
+  const y = document.querySelectorAll('.complete');
+  
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = "block";
+  }
+  
+}
+
+
+
+
+const showIncomplete = () => {
+  const x = document.querySelectorAll('.complete');
+  const y = document.querySelectorAll('.incomplete');
+
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = "block";
+  }
+}
+  
+
+  // x.style.display = 'none';
+
+  // document.getElementsByClassName('incomplete').style.display = "none";
+  // const x = document.getElementsByClassName('incomplete');
+  // console.log(x);
+  
+  // if (x.style.display == "none") {
+  //   x.style.display = "block";
+  // } else {
+  //   x.style.display = "none";
   // }
+  
+  // completed.style.display = 'none';
+  console.log('I work');
+  
+  // let toDos = document.getElementById('todo-list');
+  // document.getElementById("todo-list").innerHTML = "";
+  // toDos.innerHTML = "";
+  // const myID = document.getElementById('user').value;
+  // console.log(myID);
+  // const arrayFilter = arrayOfTodos.filter(array => array.userId == myID);
+  // console.log(arrayFilter);
+
+
+
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
 
 
 /* 
